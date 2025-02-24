57c59fdc52cac5ecf126b886029e704cf2f38f4d
erikratkai <ratkaierik4@gmail.com>
2025-02-24 18:28:04 +0100

    ciklus bevezetese

--- main.js
+++ main.js
@@ -1,40 +1,34 @@
 const array = [
-    { // Fejléc cellák értékei
+    { // array tömb nulladik elemei
         th1_value: "Harc megnevezése",
         th2_value: "Szembenálló felek",
         th3_value: "Haderő"
     },
-    { // Második sor celláinak értékei
-        td1_value: "Rákóczi szabadságharc",
-        td2_value: "Kuruc",
-        td3_value: "70.000"
+    { // array tömb első elemei
+        harc: "Rákóczi szabadságharc",
+        harcolo1: "Kuruc",
+        hadero1: "70.000",
+        harcolo2: "Labanc",
+        hadero2: "60.000"
     },
-    { // Harmadik sor celláinak értékei
-        td4_value: "Labanc",
-        td5_value: "60.000"
+    { // array tömb második elemei
+        harc: "48-as szabadságharc",
+        harcolo1: "Osztrák császárság (+ Orosz birodalom)",
+        hadero1: "170.000 (+ 200.000)",
+        harcolo2: "Magyar királyság",
+        hadero2: "170.000"
     },
-    { // Negyedik sor celláinak értékei
-        td6_value: "48-as szabadságharc",
-        td7_value: "Osztrák császárság (+ Orosz birodalom)",
-        td8_value: "170.000 (+ 200.000)"
+    { // array tömb harmadik elemei
+        harc: "I. világháború",
+        harcolo1: "Antant",
+        hadero1: "43 millió",
+        harcolo2: "Központi hatalmak",
+        hadero2: "25 millió"
     },
-    { // Ötödik sor celláinak értékei
-        td9_value: "Magyar királyság",
-        td10_value: "170.000"
-    },
-    { // Hatodik sor celláinak értékei
-        td11_value: "I. világháború",
-        td12_value: "Antant",
-        td13_value: "43 millió"
-    },
-    { // Hetedik sor celláinak értékei
-        td14_value: "Központi hatalmak",
-        td15_value: "25 millió"
-    },
-    { // Nyolcadik sor celláinak értékei
-        td16_value: "Bosworthi csata",
-        td17_value: "Angolok (York + Lancester)",
-        td18_value: "15.000"
+    { // array tömb negyedik elemei
+        harc: "Bosworthi csata",
+        harcolo1: "Angolok (York + Lancester)",
+        hadero1: "15.000"
     }
 ]
 
@@ -62,98 +56,34 @@ tr1.appendChild(th3); // Harmadik oszlopcím hozzáadása a sorhoz
 const tbody = document.createElement("tbody"); // Táblázat törzsének létrehozása
 table.appendChild(tbody); // Táblázat törzsének hozzáadása a táblázathoz
 
-const tr2 = document.createElement("tr"); // Első sor létrehozása a törzsben
-tbody.appendChild(tr2); // Első sor hozzáadása a törzshöz
+for (let i = 1; i < array.length; i++) { // for ciklus bevezetése
+    const tbody = document.createElement("tbody")
+    table.appendChild(tbody) 
 
-const td1 = document.createElement("td"); // Első cella létrehozása
-td1.innerHTML = array[1].td1_value; // Cella tartalmának beállítása
-td1.rowSpan = 2; // A cella két sort foglal el
-tr2.appendChild(td1); // Cella hozzáadása a sorhoz
+    const elem = array[i]; 
+    const tr = document.createElement("tr")
+    tbody.appendChild(tr)
+    const tr2 = document.createElement("tr")
+    tbody.appendChild(tr2) 
 
-const td2 = document.createElement("td"); // Második cella létrehozása
-td2.innerHTML = array[1].td2_value; // Cella tartalmának beállítása
-tr2.appendChild(td2); // Cella hozzáadása a sorhoz
+    const harc = document.createElement("td")
+    harc.innerHTML = elem.harc
+    harc.rowSpan = 2 
+    tr.appendChild(harc)
 
-const td3 = document.createElement("td"); // Harmadik cella létrehozása
-td3.innerHTML = array[1].td3_value; // Cella tartalmának beállítása
-tr2.appendChild(td3); // Cella hozzáadása a sorhoz
+    const harcolo1 = document.createElement("td") 
+    harcolo1.innerHTML = elem.harcolo1
+    tr.appendChild(harcolo1) 
 
-const tr3 = document.createElement("tr"); // Második sor létrehozása
-tbody.appendChild(tr3); // Második sor hozzáadása a törzshöz
+    const hadero1 = document.createElement("td") 
+    hadero1.innerHTML = elem.hadero1
+    tr.appendChild(hadero1) 
 
-const td4 = document.createElement("td"); // Negyedik cella létrehozása
-td4.innerHTML = array[2].td4_value; // Cella tartalmának beállítása
-tr3.appendChild(td4); // Cella hozzáadása a sorhoz
+    const harcolo2 = document.createElement("td")
+    harcolo2.innerHTML = elem.harcolo2 
+    tr2.appendChild(harcolo2) 
 
-const td5 = document.createElement("td"); // Ötödik cella létrehozása
-td5.innerHTML = array[2].td5_value; // Cella tartalmának beállítása
-tr3.appendChild(td5); // Cella hozzáadása a sorhoz
-
-const tr4 = document.createElement("tr"); // Harmadik sor létrehozása
-tbody.appendChild(tr4); // Harmadik sor hozzáadása a törzshöz
-
-const td6 = document.createElement("td"); // Hatodik cella létrehozása
-td6.innerHTML = array[3].td6_value; // Cella tartalmának beállítása
-td6.rowSpan = 2; // A cella két sort foglal el
-tr4.appendChild(td6); // Cella hozzáadása a sorhoz
-
-const td7 = document.createElement("td"); // Hetedik cella létrehozása
-td7.innerHTML = array[3].td7_value; // Cella tartalmának beállítása
-tr4.appendChild(td7); // Cella hozzáadása a sorhoz
-
-const td8 = document.createElement("td"); // Nyolcadik cella létrehozása
-td8.innerHTML = array[3].td8_value; // Cella tartalmának beállítása
-tr4.appendChild(td8); // Cella hozzáadása a sorhoz
-
-const tr5 = document.createElement("tr"); // Negyedik sor létrehozása
-tbody.appendChild(tr5); // Negyedik sor hozzáadása a törzshöz
-
-const td9 = document.createElement("td"); // Kilencedik cella létrehozása
-td9.innerHTML = array[4].td9_value; // Cella tartalmának beállítása
-tr5.appendChild(td9); // Cella hozzáadása a sorhoz
-
-const td10 = document.createElement("td"); // Tizedik cella létrehozása
-td10.innerHTML = array[4].td10_value; // Cella tartalmának beállítása
-tr5.appendChild(td10); // Cella hozzáadása a sorhoz
-
-const tr6 = document.createElement("tr"); // Ötödik sor létrehozása
-tbody.appendChild(tr6); // Ötödik sor hozzáadása a törzshöz
-
-const td11 = document.createElement("td"); // Tizenegyedik cella létrehozása
-td11.innerHTML = array[5].td11_value; // Cella tartalmának beállítása
-td11.rowSpan = 2; // A cella két sort foglal el
-tr6.appendChild(td11); // Cella hozzáadása a sorhoz
-
-const td12 = document.createElement("td"); // Tizenkettedik cella létrehozása
-td12.innerHTML = array[5].td12_value; // Cella tartalmának beállítása
-tr6.appendChild(td12); // Cella hozzáadása a sorhoz
-
-const td13 = document.createElement("td"); // Tizenharmadik cella létrehozása
-td13.innerHTML = array[5].td13_value; // Cella tartalmának beállítása
-tr6.appendChild(td13); // Cella hozzáadása a sorhoz
-
-const tr7 = document.createElement("tr"); // Hatodik sor létrehozása
-tbody.appendChild(tr7); // Hatodik sor hozzáadása a törzshöz
-
-const td14 = document.createElement("td"); // Tizennegyedik cella létrehozása
-td14.innerHTML = array[6].td14_value; // Cella tartalmának beállítása
-tr7.appendChild(td14); // Cella hozzáadása a sorhoz
-
-const td15 = document.createElement("td"); // Tizenötödik cella létrehozása
-td15.innerHTML = array[6].td15_value; // Cella tartalmának beállítása
-tr7.appendChild(td15); // Cella hozzáadása a sorhoz
-
-const tr8 = document.createElement("tr"); // Hetedik sor létrehozása
-tbody.appendChild(tr8); // Hetedik sor hozzáadása a törzshöz
-
-const td16 = document.createElement("td"); // Tizenhatodik cella létrehozása
-td16.innerHTML = array[7].td16_value; // Cella tartalmának beállítása
-tr8.appendChild(td16); // Cella hozzáadása a sorhoz
-
-const td17 = document.createElement("td"); // Tizenhetedik cella létrehozása
-td17.innerHTML = array[7].td17_value; // Cella tartalmának beállítása
-tr8.appendChild(td17); // Cella hozzáadása a sorhoz
-
-const td18 = document.createElement("td"); // Tizennyolcadik cella létrehozása
-td18.innerHTML = array[7].td18_value; // Cella tartalmának beállítása
-tr8.appendChild(td18); // Cella hozzáadása a sorhoz
+    const hadero2 = document.createElement("td")
+    hadero2.innerHTML = elem.hadero2
+    tr2.appendChild(hadero2)
+}
