8904f10f87e18e3f34d36dc583cfe2121f7b1f13
erikratkai <ratkaierik4@gmail.com>
2025-02-25 19:11:08 +0100

    fuggveny bevezetese

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ const array = [
         th3_value: "Haderő"
     },
     { // array tömb első elemei
-        harc: "Rákóczi szabadságharc",
+        harc_nev: "Rákóczi szabadságharc",
         harcolo1: "Kuruc",
         hadero1: "70.000",
         harcolo2: "Labanc",
         hadero2: "60.000"
     },
     { // array tömb második elemei
-        harc: "48-as szabadságharc",
+        harc_nev: "48-as szabadságharc",
         harcolo1: "Osztrák császárság (+ Orosz birodalom)",
         hadero1: "170.000 (+ 200.000)",
         harcolo2: "Magyar királyság",
         hadero2: "170.000"
     },
     { // array tömb harmadik elemei
-        harc: "I. világháború",
+        harc_nev: "I. világháború",
         harcolo1: "Antant",
         hadero1: "43 millió",
         harcolo2: "Központi hatalmak",
         hadero2: "25 millió"
     },
     { // array tömb negyedik elemei
-        harc: "Bosworthi csata",
+        harc_nev: "Bosworthi csata",
         harcolo1: "Angolok (York + Lancester)",
         hadero1: "15.000"
     }
@@ -58,8 +58,6 @@ function fuggveny(){ //fuggveny függvény/function bevezetése
     table.appendChild(tbody); // Táblázat törzsének hozzáadása a táblázathoz
 
     for (let i = 1; i < array.length; i++) { // for ciklus bevezetése
-        const tbody = document.createElement("tbody") // Törzs létrehozása minden sorhoz
-        table.appendChild(tbody) // Törzs hozzáadása a táblázathoz
 
         const elem = array[i]; // Az aktuális elem, amivel dolgozunk a ciklusban
         const tr = document.createElement("tr") // Első sor létrehozása
@@ -69,7 +67,7 @@ function fuggveny(){ //fuggveny függvény/function bevezetése
         tbody.appendChild(tr2) // Második sor hozzáadása a törzshöz
 
         const harc = document.createElement("td") // Cella létrehozása a harc nevéhez
-        harc.innerHTML = elem.harc // A cella tartalmának beállítása az adatból
+        harc.innerHTML = elem.harc_nev // A cella tartalmának beállítása az adatból
         harc.rowSpan = 2 // A cella két soron jelenik meg
         tr.appendChild(harc) // Cella hozzáadása az első sorhoz
 
@@ -95,3 +93,32 @@ function fuggveny(){ //fuggveny függvény/function bevezetése
     }
 }
 fuggveny();
+
+// const form = document.getElementById("form");
+
+// form.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     const harc_nev = document.getElementById("harc_nev"); // harc lekérése
+//     const harcolo1 = document.getElementById("harcolo1"); // harcolo1 lekérése
+//     const hadero1 = document.getElementById("hadero1");  // hadero1 lekérése
+//     const harcolo2 = document.getElementById("harcolo2"); // harcolo2 lekérése
+//     const hadero2 = document.getElementById("hadero2"); // hadero2 lekérése
+
+//     const harc_nev_value = harc_nev.value // a "harc" értékét kiszervezem egy új "harc_value" változóba
+//     const harcolo1_value = harcolo1.value // a "harcolo1" értékét kiszervezem egy új "harcolo1_value" változóba
+//     const hadero1_value = hadero1.value // a "hadero1" értékét kiszervezem egy új "hadero1_value" változóba
+//     const harcolo2_value = harcolo2.value // a "harcolo2" értékét kiszervezem egy új "harcolo2_value" változóba
+//     const hadero2_value = hadero2.value // a "hadero2" értékét kiszervezem egy új "hadero2_value" változóba
+
+//     const obj =  { // egy új objektumba rakom a value-s dolgokat
+//         td1: harc_nev_value, 
+//         td2: harcolo1_value, 
+//         td3: hadero1_value, 
+//         td4: harcolo2_value,
+//         td5: hadero2_value 
+//     }
+
+//     array.push(obj);
+//     table.innerHTML = "";
+//     fuggveny();
+// })
